c64e691b58f626114f6311a2176d151aed2ae70c
Somdotta07 <jhilick.s@gmail.com>
2021-12-13 11:35:13 +0100

    Set up with linters and empty navigation files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import {
+  BrowserRouter as Router, Route, Routes, Link,
+} from 'react-router-dom';
+import Rocket from './Components/RocketPage';
+import Mission from './Components/MissionPage';
+import MyProfile from './Components/MyProfile';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Rocket</Link>
+              </li>
+              <li>
+                <Link to="/Mission">Mission</Link>
+              </li>
+              <li>
+                <Link to="/MyProfile"> My Profile</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Rocket />} />
+            <Route path="/Mission" element={<Mission />} />
+            <Route path="/MyProfile" element={<MyProfile />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
