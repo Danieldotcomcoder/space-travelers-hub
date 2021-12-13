17be1928d169814256d159eff83a4317c5632fe6
Somdotta07 <jhilick.s@gmail.com>
2021-12-13 15:09:55 +0100

    fetch Rocket data and render UI

--- src/Components/RocketPage.js
+++ src/Components/RocketPage.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRockets } from '../Redux/Rockets/rockets';
 
 export default function Rocket() {
+  const dispatch = useDispatch();
+  const rocketNum = useSelector((state) => state.rockets);
+  useEffect(() => {
+    if (rocketNum.length === 0) {
+      dispatch(getRockets());
+    }
+  }, []);
+
   return (
     <div>
-      <h1>Hello</h1>
+      {rocketNum.map((rocket) => (
+        <ul key={rocket.id}>
+          <li>
+            <img alt="rocket" src={rocket.flickr_images} />
+          </li>
+          <div>
+            <li>{rocket.name}</li>
+            <p>
+              {rocket.reserved && (<span>Reserved</span>)}
+              {rocket.description}
+            </p>
+            <button type="button">Reserve</button>
+          </div>
+        </ul>
+      ))}
     </div>
   );
 }
