306d69c351ed38debd2b0f72bb6f9872dba332ce
Somdotta07 <jhilick.s@gmail.com>
2021-12-14 10:37:00 +0100

    Add button action for reserve and cancel

--- src/Components/RocketPage.js
+++ src/Components/RocketPage.js
@@ -1,15 +1,24 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getRockets } from '../Redux/Rockets/rockets';
+import { getRockets, reserveRockets, cancelRockets } from '../Redux/Rockets/rockets';
 
 export default function Rocket() {
   const dispatch = useDispatch();
   const rocketNum = useSelector((state) => state.rockets);
+  const reserveHandler = (id) => {
+    dispatch(reserveRockets(id));
+  };
+  const cancelHandler = (id) => {
+    dispatch(cancelRockets(id));
+  };
+
   useEffect(() => {
     if (rocketNum.length === 0) {
       dispatch(getRockets());
     }
   }, []);
+
+  let reserved = null;
 
   return (
     <div className="rocket-c">
@@ -21,10 +30,26 @@ export default function Rocket() {
           <div className="rocket-info">
             <li><h3>{rocket.name}</h3></li>
             <p className="rocket-des">
-              {rocket.reserved && (<span>Reserved</span>)}
+              {rocket.reserved && (<span className="reserved-r">Reserved</span>)}
               {rocket.description}
             </p>
-            <button className="reserve-btn" type="button">Reserve Rocket</button>
+            <button
+              type="button"
+              className={!rocket.reserved === true ? 'reserve-btn' : 'cancel-btn'}
+              onClick={
+              () => {
+                reserved = rocket.reserved;
+                if (!reserved) {
+                  reserveHandler(rocket.id);
+                } else {
+                  cancelHandler(rocket.id);
+                }
+              }
+            }
+            >
+              {!rocket.reserved === true ? 'Reserved Rocket' : 'Cancel Reservation'}
+
+            </button>
           </div>
         </ul>
       ))}
