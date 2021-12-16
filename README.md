f32a196f072044aecaa9e9c388fdf9ce4e00c561
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2021-12-16 14:37:13 +0200

    Fix Issues

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Space-Travelers-Hub
 - In this task, we will be working with the real live data from the SpaceX API. Our task is to build a web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions.
 # Screenshots
-
+## Rocket and Home Page: 
 ![image](https://user-images.githubusercontent.com/87448628/146329774-2339b30f-ff2a-451f-b9a1-8c4ed53a01a5.png)
 
+## Missions Page:
 ![image](https://user-images.githubusercontent.com/87448628/146329869-7ff101f3-9864-4ef2-a280-248cfce284b3.png)
 
+## Dragons Page:
 ![image](https://user-images.githubusercontent.com/87448628/146329918-5a122b3f-f6d5-4695-8791-26cf7fbc09f2.png)
 ## Built With
 - React-Redux
@@ -22,7 +24,7 @@
 ## Getting Started
 
  To clone this project :
-*Run command: $git clone [repo](https://github.com/Danieldotcomcoder/space-travelers-hub)
+*Run command: $ git clone [repo](https://github.com/Danieldotcomcoder/space-travelers-hub)
 *Or press the green Code button in the upper right corner and choose to download from the Download ZIP link.
 *Open terminal and type :  1-  npm install
                            2-  npm run build.
--- src/Components/tests/App.test.js
+++ src/Components/tests/App.test.js
@@ -1,9 +1,6 @@
-/* eslint-disable prefer-destructuring */
 import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
-import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
 import App from '../../App';
 import store from '../../Redux/configureStore';
 
@@ -13,31 +10,9 @@ const APP = (
   </Provider>
 );
 
-let link = null;
-let result = null;
-
 describe('App test', () => {
   it('Render App', () => {
     const app = renderer.create(APP).toJSON();
     expect(app).toMatchSnapshot();
-  });
-
-  describe('Specfic Page should appear when the corresponding button is clicked', () => {
-    it('When mission button link is clicked', () => {
-      render(APP);
-      link = screen.getByText('Mission');
-      userEvent.click(link);
-      result = screen.getAllByText(/Mission/i)[1];
-      expect(result.textContent).toBe('Mission');
-    });
-    it('When rocket button link is clicked', () => {
-      render(APP);
-      link = screen.getByText(/My profile/i);
-      userEvent.click(link);
-      result = screen.getAllByText(/Rocket/i)[1];
-      expect(result.textContent).toBe('My Rockets');
-      result = screen.getAllByText(/Mission/i)[1];
-      expect(result.textContent).toBe('My Missions');
-    });
   });
 });
